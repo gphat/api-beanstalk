a69d1fde97f5e406201cdc0193c587aa01d0b19e
Cory Watson <gphat@Cory-Watsons-Mac-Pro.local>
2011-06-11 13:43:45 -0500

    First commit.

---
+++ lib/API/Beanstalk.pm
@@ -0,0 +1,126 @@
+package API::Beanstalk;
+use Moose;
+
+use Data::Dumper;
+use LWP::UserAgent;
+use URI;
+use XML::Simple qw(XMLin);
+
+# ABSTRACT: Profile based data verification with Moose type constraints.
+
+=head1 SYNOPSIS
+
+    use API::Beanstalk;
+
+=head1 DESCRIPTION
+
+Beanstalk XXX
+
+=attr account_name
+
+The account name to use when sending requests in.
+
+=cut
+
+has 'account_name' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+has '_client' => (
+    is => 'ro',
+    isa => 'LWP::UserAgent',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $ua = LWP::UserAgent->new();
+        $ua->credentials(
+            $self->account_name.'.beanstalkapp.com:443', 'Web Password',
+            $self->username, $self->password
+        );
+        return $ua;
+    }
+);
+
+=attr password
+
+The password to use when logging in.
+
+=cut
+
+has 'password' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+=attr url
+
+The URL to use when working with the api.  Defaults to
+
+  https://$accountname.beanstalkapp.com
+  
+=cut
+
+has 'url' => (
+    is => 'ro',
+    isa => 'URI',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        return URI->new('https://'.$self->account_name.'.beanstalkapp.com/');
+    }
+);
+
+=attr username
+
+The username to use when logging in.
+
+=cut
+
+has 'username' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+=method account
+
+Get Account information.
+
+=cut
+
+sub account {
+    my ($self) = @_;
+
+    my $resp = $self->_client->get($self->url."api/account.xml");
+    my $data = XMLin($resp->decoded_content);
+
+    return {
+        'third-level-domain' => $data->{'third-level-domain'},
+        'name' => $data->{name},
+        'plan-id' => $data->{'plan-id'}->{content},
+        'time-zone' => $data->{'time-zone'},
+        'owner-id' => $data->{'owner-id'}->{content},
+        'suspended' => $data->{suspended}->{content} eq 'true' ? 1 : 0,
+        'id' => $data->{id}->{content},
+        'updated-at' => $data->{'updated-at'}->{content}
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+
+=begin :postlude
+
+=head1 CONTRIBUTORS
+
+Me
+
+=end :postlude
+
---
+++ t/account.t
@@ -0,0 +1,15 @@
+use Test::More;
+use strict;
+
+use API::Beanstalk;
+
+my $api = API::Beanstalk->new(
+    account_name => 'testinbeanstalk2g',
+    username => 'testbeanstalk',
+    password => $ENV{'TEST_BEANSTALK_PASS'}
+);
+
+my $data = $api->account;
+cmp_ok('testinbeanstalk2g', 'eq', $data->{'third-level-domain'}, 'content');
+
+done_testing;
